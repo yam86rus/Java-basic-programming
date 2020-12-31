82ca060f1d2860719a54952c5530960a060bb5ee
Yam86rus <yam86rus@gmail.com>
2020-12-31 11:52:29 +0500

    up

---
+++ src/Chapter19/Ex19_47.java
@@ -0,0 +1,25 @@
+package Chapter19;
+
+//Задание 19.47
+//        Напишите метод, который принимает в качестве параметров два массива
+//        целых чисел одинаковой длины и проверяет, являются ли массивы
+//        идентичными.
+public class Ex19_47 {
+    static boolean foo(int[] ar1, int[] ar2) {
+        boolean result = true;
+        for (int i = 0; i < ar1.length; i++) {
+            if (ar1[i] != ar2[i]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 5};
+        int[] b = {1, 2, 3, 4, 5};
+//        int[] b = {3, 4, 5, 6, 7};
+        System.out.println(foo(a, b));
+    }
+}
