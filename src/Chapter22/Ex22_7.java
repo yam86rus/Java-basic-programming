44c6e60d507535df4411e467b5ccf223d443c3ef
yam86rus <yam86rus@gmail.com>
2021-03-08 20:34:13 +0500

    up

---
+++ src/Chapter22/Ex22_7.java
@@ -0,0 +1,7 @@
+package Chapter22;
+// TODO Сделать завтра. а Сегодня 8-е марта.
+public class Ex22_7 {
+    public static void main(String[] args) {
+        System.out.println("8 марта!");
+    }
+}
