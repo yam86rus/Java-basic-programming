5dd8e7b086cbd6eb6e02b5d58d6e8cc08b0c36ce
Yam86rus <yam86rus@gmail.com>
2021-03-09 09:02:30 +0500

    up

--- src/Chapter22/Ex22_7.java
+++ src/Chapter22/Ex22_7.java
@@ -1,7 +1,28 @@
 package Chapter22;
-// TODO Сделать завтра. а Сегодня 8-е марта.
+
+import java.util.Arrays;
+
+//Задание 22.7
+//        Напишите метод, который получает в качестве параметра двумерный
+//        массив целых чисел.
+//        Метод возвращает массив, заполненный следующим образом:
+//        • первый (по порядку) столбец заполнен значениями 1;
+//        • второй (по порядку) столбец заполнен значениями 2 и так далее.
 public class Ex22_7 {
+    static public int[][] foo(int[][] da) {
+        for (int i = 0; i < da.length; i++) {
+            for (int j = 0; j < da[i].length; j++) {
+                da[i][j] = j + 1;
+            }
+        }
+        return da;
+    }
+
     public static void main(String[] args) {
-        System.out.println("8 марта!");
+        int[][] da = new int[5][5];
+        int[][] da1 = foo(da);
+        for (int[] i : da1) {
+            System.out.println(Arrays.toString(i));
+        }
     }
 }
